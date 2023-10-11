20d4acf958f7d0dc3398c6bd435dab2bb5a30dee
C4llSiqn <zachwilke@icloud.com>
2023-10-11 12:08:22 -0600

    modified display

--- display.py
+++ display.py
@@ -1,5 +1,7 @@
 from luma.core import cmdline
 
 def make_oled():
-    try: return cmdline.create_device()
+    parser = cmdline.create_parser(description='make_oled arguments')
+    args = parser.parse_args(None)
+    try: return cmdline.create_device(args)
     except: return None
