97ba67760c76a7b6cfbdb6e3ff0f7c13fba93c9c
C4llSiqn <zachwilke@icloud.com>
2023-11-07 11:27:35 -0700

    Google Translate

--- translationAPI.py
+++ translationAPI.py
@@ -1,19 +1,17 @@
 #!usr/bin/env python3
 
-import requests
-import json
+#import requests
+#import json
+import googletrans
+
 
 #TRANSLATE_ENDPOINT = "https://libretranslate.com/translate"
-TRANSLATE_ENDPOINT = "http://localhost:5000/translate"
+#TRANSLATE_ENDPOINT = "http://192.168.137.206:5000/translate"
+#TRANSLATE_ENDPOINT = googletrans.Translator()
 
 def translate(q: str, source: str = "auto", destination: str = "en") -> dict[str, str]:
-    data = {
-        "q": q,
-        "source": source,
-        "target": destination
-    }
-    res = requests.post(TRANSLATE_ENDPOINT, data)
-    return json.loads(res.text)
+    translator = googletrans.Translator()
+    return translator.translate(q, destination, source).text
 
 if __name__ == "__main__":
     inputtext = input("Translate Text> ")
